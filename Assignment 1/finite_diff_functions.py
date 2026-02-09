8068e813dd01e67a3153a92aad9d39398ea5edf3
Karl Johan Maastrup Kristensen <karljohan@maastrup.dk>
2026-02-09 11:31:33 +0100

    norm fucntions

--- Assignment 1/finite_diff_functions.py
+++ Assignment 1/finite_diff_functions.py
@@ -2,3 +2,8 @@ import numpy as np
 from scipy import sparse
 import matplotlib.pyplot as plt
 
+def l2_norm(u, uhat) -> float:
+    return np.sqrt(np.sum((u - uhat)**2))
+
+def inf_norm(u, uhat) -> float:
+    return np.max(np.abs(u - uhat))
