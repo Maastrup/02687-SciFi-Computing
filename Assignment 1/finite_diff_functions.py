39c35aa411750bdb1838facf51db141eb2a78166
Karl Johan Maastrup Kristensen <karljohan@maastrup.dk>
2026-02-16 11:28:40 +0100

    std functions

--- Assignment 1/finite_diff_functions.py
+++ Assignment 1/finite_diff_functions.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy import sparse
+from scipy.special import factorial
 import matplotlib.pyplot as plt
 
 def l2_norm(u, uhat) -> float:
@@ -7,3 +8,66 @@ def l2_norm(u, uhat) -> float:
 
 def inf_norm(u, uhat) -> float:
     return np.max(np.abs(u - uhat))
+
+
+def fdcoeffV_uniform(k,xbar,stencil, h):
+    n = len(stencil)
+    A = np.ones((n,n))
+    xrow = stencil
+    for i in range(1,n):
+        A[i,:]= xrow**(i)/factorial(i)
+
+    b = np.zeros((n,))
+    b[k] = 1
+    c = np.linalg.solve(A,b)
+    return 1/h**k * c
+
+def der_approx_uniform(k: int, xbar: float, h, u_func, stencil):
+    coeff = fdcoeffV_uniform(k,xbar,stencil, h)
+    U = u_func(xbar+h*stencil)
+    #print(U)
+    der = np.dot(coeff,U)
+    return der
+
+def fdcoeffV(k,xbar,x):
+    '''
+        # Parameters
+        k: derivative order
+        xbar: x[i]-value at which to approximate 
+            k-th derivative stencil coefficients
+        x: slice of x-points (x[i + stencil]) around xbar 
+            to use in approximation
+
+        # Output
+        c: Coefficients of k-th order derivative of u at xbar
+    '''
+    n = len(x)
+    A = np.ones((n,n))
+    xrow = x - xbar
+    for i in range(1,n):
+        A[i,:]= xrow**(i)/factorial(i)
+
+    b = np.zeros((n,))
+    b[k] = 1
+    c = np.linalg.solve(A,b)
+    return c
+
+def der_approx(k: int, xbar: float, x, u_func):
+    '''
+        # Parameters
+        k: derivative order
+        xbar: x[i]-value at which to approximate 
+            k-th derivative stencil coefficients
+        x: slice of x-points (x[i + stencil]) around xbar 
+            to use in approximation
+        u_func: function to approximate
+
+        # Output
+        c: Coefficients of k-th order derivative of u at xbar
+    '''
+    coeff = fdcoeffV(k,xbar,x)
+    U = u_func(x)
+    #print(U)
+    der = np.dot(coeff,U)
+    return der
+
