69149c9aa6d8dc03df973c544e884d3357c6e519
Oliver Lorentzen <O_lorentzen@hotmail.com>
2026-05-04 11:09:23 +0200

    egrd

---
+++ Code/Exercise3.py
@@ -0,0 +1,155 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import sparse
+from scipy.sparse.linalg import spsolve
+from finite_diff_functions import l2_norm_grid, smooth, coarsen, scatter
+
+# MoMS solution to the Poisson problem in 2D
+def u_func(x,y):
+    return np.sin(4*np.pi*(x+y)) + np.cos(4*np.pi*x*y)
+
+def f_func(x,y):
+    term1 = -32*np.pi**2*np.sin(4*np.pi*(x+y))
+    term2 = -16*np.pi**2*(x**2 + y**2)*np.cos(4*np.pi*x*y)
+    return term1 + term2
+
+# LHS of a discretized Poisson problem
+def Amult(U, m):
+    h = 1.0 / (m + 1)
+    AU = np.zeros_like(U)
+
+    for j in range(m):
+        for i in range(m):
+            k = i + j*m   # 2D -> 1D index
+            
+            center = U[k]
+
+            left = U[k-1] if i > 0 else 0.0
+            right = U[k+1] if i < m - 1 else 0.0
+            down = U[k-m] if j > 0 else 0.0
+            up = U[k+m] if j < m - 1 else 0.0
+
+            # Discrete Laplacian (Δ_h): (neighbors - 4*center)/h^2
+            AU[k] = (left + right + down + up - 4 * center) / h**2
+
+    return AU
+
+def form_rhs(m, f_func, u_func):
+    '''
+        Form the discretized RHS of the INTERIOR points of a Poisson problem in 2D with rectangular domain
+        
+        # INPUTS
+            m: number of INTERIOR points in the discretized domain
+            f_func: RHS as a analytical function of (x,y) of the BVP
+            u_func: DC boundary as analytical function of (x,y)
+
+        # OUTPUT
+            b: discretized RHS of the interior points in the discretized domain
+    '''
+
+    h = 1.0/(m+1)
+    b = np.zeros(m**2)
+
+    for j in range(m):
+        for i in range(m):
+
+            k = j*m + i
+            x = (i+1)*h
+            y = (j+1)*h
+
+            b[k] = f_func(x,y)
+
+            # left boundary
+            if i == 0:
+                b[k] -= u_func(0,y)/h**2
+
+            # right boundary
+            if i == m-1:
+                b[k] -= u_func(1,y)/h**2
+
+            # bottom boundary
+            if j == 0:
+                b[k] -= u_func(x,0)/h**2
+
+            # top boundary
+            if j == m-1:
+                b[k] -= u_func(x,1)/h**2
+
+    return b
+
+
+## VCYCLE
+def vcycle(A, R, P, u, f, l):
+    '''
+        # Parameters
+            A: A matrix free operator computing discretized Poisson matrix-vector product A(u) = -A*u
+            R: Function | (u, m) -> u with len(u) = m//2 Restriction operator coarsening the grid vectors to length m/2 x m/2
+            P: Function | (u, m) -> u with len(u) = m**2 Prolongation operator interpolating grid vectors from length m/2 x m/2 -> m x m
+            u: current guess at solution to Poisson problem
+            f: RHS of discretized Poisson problem
+            l: initial number of nodes along x- and y-axis
+        # Output
+            u: 
+    '''
+    if l == 1:
+        # l = 1 ==> A^-1 = [-h**2/4] ==> u = u[0] = -h**2 / 4 * f[0]
+        # l = 1 ==> h = 1/2
+        #       ==> u = u[0] = -1/8 * f[0]
+        u[0] = -1 / 16 * f[0]
+    else:
+        for _ in range(3): # nu_pre in algorithm
+            u = smooth(u, 0.65306, 2**l - 1, f)
+        
+        # Residual for Δ_h u = f
+        r = f - A(u, 2**l - 1)
+        r = R(r, 2**l - 1)
+        e = np.zeros_like(r)
+        e = vcycle(A, R, P, e, r, l - 1)
+        e = P(e, 2**l - 1)
+        u += e
+        
+        for _ in range(3):
+            u = smooth(u, 0.65306, 2**l - 1, f)
+    
+    return u
+
+# multi grid loop
+l = 5 # levels
+m = 2**l - 1
+print(f"m = {m}")
+x = np.linspace(0, 1, m)
+y = np.linspace(0,1, m)
+X,Y = np.meshgrid(x, y, indexing='ij')
+U0 = (1 + 2*X + 2*Y)
+for i in range(m):
+    for j in range(m):
+        assert U0[i,j] - U0.flatten()[i + j*m] < 1e-15
+
+U0 = U0.flatten()
+F = form_rhs(m, f_func, u_func)
+assert F.shape == U0.shape
+
+kmax = 10
+convergence_history = []
+
+k = 0
+rk = F - Amult(U0, m)  # residual r = f - Δ_h u
+Uk = np.copy(U0)
+EPS = 1e-4
+for k in range(kmax):
+    norm = l2_norm_grid(rk, 1/(m+1)) # h = 1/m+1
+    convergence_history.append(norm)
+    print(f"||r||_2: {norm}")
+    if norm < EPS: break
+
+    Uk = vcycle(Amult, coarsen, scatter, Uk, F, l)
+    rk = F - Amult(Uk, m)
+    
+
+
+plt.semilogy(convergence_history, '-o')
+plt.xlabel('iterations')
+plt.ylabel(r'$||r||_2$')
+plt.grid()
+plt.title('Convergence of Mulitgrid Vcycle solver')
+plt.show()
---
+++ Code/finite_diff_functions.py
@@ -0,0 +1,170 @@
+import numpy as np
+from scipy import sparse
+from scipy.special import factorial
+import matplotlib.pyplot as plt
+
+def l2_norm(v) -> float:
+    return np.sqrt(np.sum((v)**2))
+
+def l2_norm_grid(v, h: float) -> float:
+    return h * l2_norm(v)
+
+def inf_norm(v) -> float:
+    return np.max(np.abs(v))
+
+def inf_norm_grid(v,h:float) -> float:
+    return h**2 * inf_norm(v)
+
+
+def fdcoeffV_uniform(k: int, h: float, stencil: np.ndarray):
+    n = len(stencil)
+    A = np.ones((n,n))
+    for i in range(1,n):
+        A[i,:]= stencil**(i)/factorial(i)
+
+    b = np.zeros((n,))
+    b[k] = 1
+    c = np.linalg.solve(A,b)
+    return 1/h**k * c
+
+def der_approx_uniform(k: int, xbar: float, h, u_func, stencil):
+    coeff = fdcoeffV_uniform(k,h,stencil)
+    U = u_func(xbar+h*stencil)
+    #print(U)
+    der = np.dot(coeff,U)
+    return der
+
+def fdcoeffV(k: int, xbar, x):
+    '''
+        # Parameters
+        k: derivative order
+        xbar: x[i]-value at which to approximate 
+            k-th derivative stencil coefficients
+        x: slice of x-points (x[i + stencil]) around xbar 
+            to use in approximation
+
+        # Output
+        c: Coefficients of k-th order derivative of u at xbar
+    '''
+    n = len(x)
+    A = np.ones((n,n))
+    xrow = x - xbar
+    for i in range(1,n):
+        A[i,:]= xrow**(i)/factorial(i)
+
+    b = np.zeros((n,))
+    b[k] = 1
+    c = np.linalg.solve(A,b)
+    return c
+
+def der_approx(k: int, xbar: float, x, u_func):
+    '''
+        # Parameters
+        k: derivative order
+        xbar: x[i]-value at which to approximate 
+            k-th derivative stencil coefficients
+        x: slice of x-points (x[i + stencil]) around xbar 
+            to use in approximation
+        u_func: function to approximate
+
+        # Output
+        c: Coefficients of k-th order derivative of u at xbar
+    '''
+    coeff = fdcoeffV(k,xbar,x)
+    U = u_func(x)
+    #print(U)
+    der = np.dot(coeff,U)
+    return der
+
+
+def smooth(U, omega, m, F):
+    h = 1.0/(m+1)
+    Unew = np.copy(U)
+
+    for j in range(m):
+        for i in range(m):
+            k = i + j*m
+
+            left  = U[k-1] if i > 0   else 0.0
+            right = U[k+1] if i < m-1 else 0.0
+            down  = U[k-m] if j > 0   else 0.0
+            up    = U[k+m] if j < m-1 else 0.0
+
+            Unew[k] = (
+                (1-omega)*U[k]
+                + omega/4 * (left + right + up + down - h**2 * F[k])
+            )
+
+    return Unew
+
+def vec_smooth(U, omega, m, F):
+    h = 1/ (m+1)
+    Ugrid = U.reshape(m,m)
+    Fgrid = F.reshape(m,m)
+    Ugrid[1:-1, 1:-1] = 0.25 * (Ugrid[:-2, 1:-1] + Ugrid[1:-1, :-2]
+                       + Ugrid[2:, 1:-1] + Ugrid[1:-1, 2:]
+                       - 4 * Ugrid[1:-1, 1:-1] - h**2 * Fgrid[1:-1, 1]
+    )
+    return Ugrid.reshape(-1)
+
+def coarsen(R, m):
+    mc = m // 2
+    Rc = np.empty(mc**2)
+
+    # Half weighting
+    for i in range(mc):
+        for j in range(mc):
+            '''
+                ic = kc % mc
+                jc = kc // mc
+                k = 2ic + 2*jc*m 
+                k = 2*kc
+                k = i + j*m = 2*kc
+                (i ± 1, j) = k ± 1 = 2*kc ± 1
+                (i, j ± 1) = k ± m = 2*kc ± m
+            '''
+
+            kc = i + j*mc
+            k = (2*i + 1) + (2*j + 1)*m
+
+            center = R[k]
+            left  = R[k-1] 
+            right = R[k+1] 
+            up  = R[k-m]   
+            down    = R[k+m]
+            
+            Rc[kc] = 1 / 8 * (
+                left # (i - 1, j)
+                + up # (i, j - 1)
+                + 4 * center # (i, j)
+                + down # (i, j + 1)
+                + right # (i + 1, j)
+            )
+    return Rc
+
+def index_c_to_f(i,j):
+    return (2*i + 1, 2*j + 1)
+
+def scatter(Rc, m):
+    mc = m // 2
+    fine = np.zeros(m**2)
+    fine.reshape(m,m)[1::2, 1::2] = Rc.reshape(mc, mc)
+    for ic in range(mc):
+        for jc in range(mc):
+            i, j = index_c_to_f(ic, jc)
+            # fine indexes
+            n = i + (j-1) * m
+            s = i + (j + 1) * m
+            w = i - 1 + j*m
+            e = i + 1 + j*m
+            nw = i - 1 + (j-1) * m
+            ne = i + 1 + (j-1) * m
+            sw = i - 1 + (j+1) * m
+            se = i + 1 + (j+1) * m
+            corners = np.array([nw, ne, sw, se])
+            cross = np.array([n, s, w, e])
+
+            center_val = Rc[ic + jc*mc]
+            fine[corners] += 1/4 * center_val
+            fine[cross] += 1/2 * center_val
+    return fine
